343f795f4cf30be44fddabfaa286dae116cb3ece
David Spreekmeester <david@grrr.nl>
2017-05-30 02:01:21 +0200

    Fixed scope issue

--- mysql.js
+++ mysql.js
@@ -20,37 +20,37 @@ var tunnel = module.exports = {
         dbConfig = tunnel._addDefaults(dbConfig)
         return new Promise(function(resolve, reject) {
             tunnel._conn = new Client();
-            tunnel._conn.on('ready', tunnel._onReady).connect(sshConfig)
+            tunnel._conn.on('ready', function() {
+                tunnel._conn.forwardOut(
+                    '127.0.0.1',
+                    12345,
+                    dbConfig.host,
+                    dbConfig.port,
+                    function (err, stream) {
+                        if (err) {
+                            tunnel._conn.end()
+                            var msg = err.reason == 'CONNECT_FAILED'
+                                ? 'Connection failed.'
+                                : err
+                            return reject(err)
+                        }
+
+                        // override db host, since we're operating from within the SSH tunnel
+                        dbConfig.host = 'localhost'
+                        dbConfig.stream = stream
+
+                        var sql = mysql.createConnection(dbConfig)
+                        resolve(sql)
+                    }
+                )
+            }).connect(sshConfig)
         })
     },
 
     close: function() {
-        tunnel._conn.end()
-    },
-
-    _onReady: function() {
-        tunnel._conn.forwardOut(
-            '127.0.0.1',
-            12345,
-            dbConfig.host,
-            dbConfig.port,
-            function (err, stream) {
-                if (err) {
-                    tunnel._conn.end()
-                    var msg = err.reason == 'CONNECT_FAILED'
-                        ? 'Connection failed.'
-                        : err
-                    return reject(err)
-                }
-
-                // override db host, since we're operating from within the SSH tunnel
-                dbConfig.host = 'localhost'
-                dbConfig.stream = stream
-
-                var sql = mysql.createConnection(dbConfig)
-                resolve(sql)
-            }
-        )
+        if ('end' in tunnel._conn) {
+            tunnel._conn.end()
+        }
     },
 
     _addDefaults(dbConfig) {
